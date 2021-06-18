efa1ff4aee276b9c28975a1f61faf28846b70e7f
my-mint2021 <zzz007700zzz@gmail.com>
2021-06-18 17:46:33 +0700

    popup bez adapt

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,45 @@
 $(function () {
 
+  // ===================кнопка бургер==================
+  $('.burger').on("click", function () {
+    $('.burger').toggleClass('burger--active');
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
+  $('.menu__list').on("click", function () {
+    $('.menu__list').removeClass('menu__list--active');
+    $('.burger').removeClass('burger--active');
+  });
+
+  // ================высплывающее окно===============
+  // $('.modal-btn').on("click", function () {
+  //   $('.modal-popup').addClass('modal-popup--active')
+  //   $('.popup-overlay').addClass('popup-overlay--active')
+  // });
+
+  // $('.popup-close').on("click", function () {
+  //   $('.modal-popup').removeClass('modal-popup--active');
+  //   $('.popup-overlay').removeClass('popup-overlay--active');
+  // });
+
+  // $('.popup-overlay').on("click", function () {
+  //   $('.modal-popup').removeClass('modal-popup--active');
+  //   $('.popup-overlay').removeClass('popup-overlay--active');
+  // });
 
 
 
-  $('.modal-btn').on("click", function () {
- 
+
+  $('.btn-js').on("click", function () {
     $('.modal-popup').addClass('modal-popup--active')
     $('.popup-overlay').addClass('popup-overlay--active')
-
   });
 
   $('.popup-close').on("click", function () {
     $('.modal-popup').removeClass('modal-popup--active');
     $('.popup-overlay').removeClass('popup-overlay--active');
   });
+
   $('.popup-overlay').on("click", function () {
     $('.modal-popup').removeClass('modal-popup--active');
     $('.popup-overlay').removeClass('popup-overlay--active');
@@ -22,9 +48,10 @@ $(function () {
 
 
 
-
-
-
+  const element = document.querySelector('.popup-form__select');
+  const choices = new Choices(element, {
+    searchEnabled: false,
+  });
 
 
 });
