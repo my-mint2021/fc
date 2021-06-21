67fe4f31bb4a0f7828328da92b9dd5764cc6b656
my-mint2021 <zzz007700zzz@gmail.com>
2021-06-21 15:45:18 +0700

    pered sliderom

--- app/js/main.js
+++ app/js/main.js
@@ -12,32 +12,18 @@ $(function () {
   });
 
   // ================высплывающее окно===============
-  // $('.modal-btn').on("click", function () {
-  //   $('.modal-popup').addClass('modal-popup--active')
-  //   $('.popup-overlay').addClass('popup-overlay--active')
-  // });
 
-  // $('.popup-close').on("click", function () {
-  //   $('.modal-popup').removeClass('modal-popup--active');
-  //   $('.popup-overlay').removeClass('popup-overlay--active');
-  // });
-
-  // $('.popup-overlay').on("click", function () {
-  //   $('.modal-popup').removeClass('modal-popup--active');
-  //   $('.popup-overlay').removeClass('popup-overlay--active');
-  // });
-
-
-
+  // открытие первого попапа
 
   $('.btn-js').on("click", function () {
-    $('.modal-popup').addClass('modal-popup--active')
-    $('.popup-overlay').addClass('popup-overlay--active')
+    $('.modal-popup').addClass('modal-popup--active');
+    $('.popup-overlay').addClass('popup-overlay--active');
   });
 
   $('.popup-close').on("click", function () {
     $('.modal-popup').removeClass('modal-popup--active');
     $('.popup-overlay').removeClass('popup-overlay--active');
+    $('.modal-small').removeClass('modal-small--active');
   });
 
   $('.popup-overlay').on("click", function () {
@@ -45,8 +31,12 @@ $(function () {
     $('.popup-overlay').removeClass('popup-overlay--active');
   });
 
+  // открытие второго попапа из первого
 
-
+  $('.popup-form__btn').on("click", function () {
+    $('.modal-popup').removeClass('modal-popup--active');
+    $('.modal-small').addClass('modal-small--active');
+     });
 
   const element = document.querySelector('.popup-form__select');
   const choices = new Choices(element, {
