048ba94224392d865f10f8f034bc8f6555724a23
my-mint2021 <zzz007700zzz@gmail.com>
2021-06-22 17:30:40 +0700

    top-slider end

--- app/js/main.js
+++ app/js/main.js
@@ -36,12 +36,37 @@ $(function () {
   $('.popup-form__btn').on("click", function () {
     $('.modal-popup').removeClass('modal-popup--active');
     $('.modal-small').addClass('modal-small--active');
-     });
+  });
 
   const element = document.querySelector('.popup-form__select');
   const choices = new Choices(element, {
     searchEnabled: false,
   });
+
+  // табы  
+
+  $('.tabs__link').on("click", function (e) {
+    e.preventDefault();
+    $('.tabs__link').removeClass('tabs__link--active');
+    $(this).addClass('tabs__link--active');
+    $('.tabs__box').removeClass('tabs__box--active');
+    $($(this).attr('href')).addClass('tabs__box--active');
+  });
+
+  $('.tabs__item').on("click", function () {
+    $('.tabs__item').removeClass('tabs__item--active');
+    $(this).addClass('tabs__item--active');
+  });
+
+
+// первый слайдер
+  $('.slider-top__inner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+  });
+
+
 
 
 });
